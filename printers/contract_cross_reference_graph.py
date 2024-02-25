16b17d94a07ec5d0812e2fcf846d4bde0bd2a833
MeditationDuck <yoshinao@fit.cvut.cz>
2024-02-25 17:19:01 +0100

    feat: add options for contract-cross-reference-graph

--- printers/contract_cross_reference_graph.py
+++ printers/contract_cross_reference_graph.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections import deque, defaultdict
 from pathlib import Path
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Dict
 
 import networkx as nx
 import rich_click as click
@@ -14,12 +14,15 @@ from wake.cli import SolidityName
 from wake.printers import Printer, printer
 
 class ContractCrossReferenceGraphPrinter(Printer):
+    _names: Set[str]
     _out: Path
     _direction: str
     _links: bool
     _force: bool
     _inherit: bool
-    # _single_file: bool
+    _single_file: bool
+    _referrer: bool
+    _referring: bool
 
     _contracts: List[ir.ContractDefinition]
     
@@ -37,24 +40,21 @@ class ContractCrossReferenceGraphPrinter(Printer):
             node = node.parent
 
 
-    def print(self) -> None:       
+    def print(self) -> None:   
+
+        for name in self._names:
+            print(name)
+            
         import graphviz as gv
 
-        if not self._force and self._out.exists():
-            self.logger.warning(f"File {self._out} already exists, skipping")
-            return
-
-        g = gv.Digraph("Contract cross reference")
-        g.attr(rankdir=self._direction)
-        g.attr("node", shape="box")
-
         edges: Set[Tuple[str, str]] = set()
+        nodes: Set[Tuple[str, str, str]] = set()
+        # contract_id, contract_name, URL(contract_source)
 
         for contract in self._contracts:
-            node_attrs = {}
-            if self._links:
-                node_attrs["URL"] = self.generate_link(contract)
-            g.node(f"{contract.parent.source_unit_name}_{contract.name}", contract.name, **node_attrs)
+            node_tuple = (f"{contract.parent.source_unit_name}_{contract.name}", contract.name, self.generate_link(contract))
+            nodes.add(node_tuple)
+
 
         for node in self._contracts:
             node_id = f"{node.parent.source_unit_name}_{node.name}"
@@ -114,14 +114,91 @@ class ContractCrossReferenceGraphPrinter(Printer):
             #             edges.add(edge_id)
 
 
-        for from_, to in edges:
-            g.edge(from_, to)
-        g.save(self._out)
+        if self._single_file and len(self._names) == 0:
+            g = gv.Digraph("Contract cross reference")
+            g.attr(rankdir=self._direction)
+            g.attr("node", shape="box")
+            for v in nodes:
+                g.node(v[0], v[1], URL=v[2])
+            for from_, to in edges:
+                g.edge(from_, to)
+            p = self._out / "contract-cross-reference-graph.dot"
+            if not self._force and p.exists():
+                self.logger.warning(f"File {p} already exists, skipping")
+                return
+            g.save(p)
+        elif not self._single_file:
+            for node in nodes:
+                if node[1] not in self._names:
+                    continue
+                g = gv.Digraph("Contract cross reference")
+                g.attr(rankdir=self._direction)
+                g.attr("node", shape="box")
+                added_nodes = set()
+
+                style="filled"
+                g.node(node[0], node[1], URL=node[2], style=style)
+                added_nodes.add(node[0])
+
+                for edge in edges:
+                    source, target = edge[0], edge[1]
+                    if (self._referrer and target == node[0]) or (self._referring and source == node[0]):
+                        for node_id, ct_name, url in nodes:
+                            if (node_id == source  or node_id == target) and node_id not in added_nodes:
+                                g.node(node_id, ct_name, URL=url)
+                                added_nodes.add(node_id)
+                        if source in added_nodes and target in added_nodes:
+                            g.edge(source, target)
+
+                p = self._out / f"contract-cross-reference-graph-{node[1]}.dot"
+                if not self._force and p.exists():
+                    self.logger.warning(f"File {p} already exists, skipping")
+                    continue
+                g.save(p)
+        else: # single file and there is names
+            g = gv.Digraph("Contract cross reference")
+            g.attr(rankdir=self._direction)
+            g.attr("node", shape="box")
+            added_nodes = set()
+            for node in nodes:
+                if node[1] not in self._names: # only for names distingish by the name of contract
+                    continue
+                
+                if node[0] not in added_nodes: # generally distingish by the id 
+                    style="filled"
+                    g.node(node[0], node[1], URL=node[2], style=style)
+                    added_nodes.add(node[0])
+
+                for edge in edges:
+                    source, target = edge[0], edge[1]
+                    if (self._referrer and target == node[0]) or (self._referring and source == node[0]):
+                        for node_id, ct_name, url in nodes:
+                            if (node_id == source  or node_id == target) and node_id not in added_nodes:
+                                style = "filled" if ct_name in self._names else ""
+                                g.node(node_id, ct_name, URL=url, style=style)
+                                added_nodes.add(node_id)
+                        if source in added_nodes and target in added_nodes:
+                            g.edge(source, target)
+            p = self._out / "contract-cross-reference-graph.dot"
+            if not self._force and p.exists():
+                self.logger.warning(f"File {p} already exists, skipping")
+                return
+            g.save(p)
+                
 
     def visit_contract_definition(self, node:ir.ContractDefinition):
         self._contracts.append(node)
     
     @printer.command(name="contract-cross-reference-graph")
+    @click.option(
+        "--name",
+        "-n",
+        "names",
+        type=SolidityName("contract", case_sensitive=False),
+        multiple=True,
+        help="Contract names",
+    )
+
     @click.option(
         "--force",
         "-f",
@@ -133,9 +210,9 @@ class ContractCrossReferenceGraphPrinter(Printer):
         "-o",
         "--out",
         is_flag=False,
-        default=".wake/contract-cross-reference-graph.dot",
-        type=click.Path(file_okay=True, dir_okay=False, writable=True),
-        help="Output file",
+        default=".wake/contract-cross-reference-graphs",
+        type=click.Path(file_okay=False, dir_okay=True, writable=True),
+        help="Output directory",
     )
     @click.option(
         "--direction",
@@ -154,45 +231,48 @@ class ContractCrossReferenceGraphPrinter(Printer):
         default=False,
         help="Include inheritance in cross-reference graph.",
     )
-    # @click.option(
-    #     "--single-file/--multiple-files",
-    #     default=True,
-    #     help="Generate single with all discovered contracts or multiple files per contract",
-    # )
-    # @click.option(
-    #     "--referer/--no-referer",
-    #     default=True,
-    #     help="Generate contract referer",
-    # )
-    # @click.option(
-    #     "--refering/--no-refering",
-    #     default=True,
-    #     help="Generate contract refering",
-    # )
+    @click.option(
+        "--single-file/--multiple-files",
+        default=True,
+        help="Generate single with all discovered contracts or multiple files per contract",
+    )
 
+    @click.option(
+        "--referrer/--no-referrer",
+        default=True,
+        help="Generate contracts that refer to the contract.",
+    )
+    @click.option(
+        "--referring/--no-referring",
+        default=True,
+        help="Generate contracts that are referenced by the contract.",
+    )
 
     
     def cli(
         self,
+        names: Tuple[str, ...],
         force: bool,
         out: str,
         direction: str,
         links: bool,
         inherit: bool,
-        # single_file: bool,
-        # referer: bool,
-        # refering: bool,
-
+        single_file: bool,
+        referrer: bool,
+        referring: bool,
     ) -> None:
         """
         Generate contract cross reference graph.
         """
+        self._names = names
         self._force = force
         self._out = Path(out).resolve()
-        self._out.parent.mkdir(parents=True, exist_ok=True)
+        self._out.mkdir(parents=True, exist_ok=True)
         self._direction = direction
         self._links = links
         self._inherit = inherit
-        # self._single_file = single_file
+        self._single_file = single_file
+        self._referrer = referrer
+        self._referring = referring
 
 
